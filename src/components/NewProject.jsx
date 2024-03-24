ff3f69fdef1f3f9dc8cae73624056526764e29a6
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-03-24 15:00:02 +0300

    styling inputs and buttons with tailwindcss

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
+import Input from './Input';
 
 function NewProject() {
   return (
-    <div>
-      <menu>
-        <li><button>Cancel</button></li>
-        <li><button>Save</button></li>
+    <div className='w-[35rem] mt-16'>
+      <menu className='flex items-center justify-end gap-4 my-4'>
+        <li>
+          <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
+        </li>
+        <li>
+          <button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button>
+        </li>
       </menu>
       <div>
-        <p>
-          <label htmlFor="">Title</label>
-          <input type="text" />
-        </p>
-        <p>
-          <label htmlFor="">Description</label>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </p>
-        <p>
-          <label htmlFor="">Due Date</label>
-          <input type="date" />
-        </p>
+        <Input label="title" />
+        <Input label="Description" textarea />
+        <Input label="Due Date" />
       </div>
     </div>
   );
