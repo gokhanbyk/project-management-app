b16a8823e92250888f03d36191a8cea15f97a7a0
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-03-24 14:40:24 +0300

    project sidebar component starter

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
+
+export default ProjectsSidebar;
